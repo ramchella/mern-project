771af4666277bca725ae9c81ba8353eff4cda66b
Your Name <you@example.com>
2021-08-02 21:46:40 -0500

    aa

--- server/index.js
+++ server/index.js
@@ -5,15 +5,13 @@ import cors from 'cors';
 import studentRoutes from './routes/student.js';
 
 const app = express();
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use('/students', studentRoutes);
-
 app.use(cors());
+
+app.use('/students', studentRoutes);
 
 const CONNECTION_URL = "mongodb+srv://ramtest:ramtest@cluster0.emnl8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
